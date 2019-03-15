741e47011b0e35accc24e77d69b4a7704fd00b38
chenhewen <chenhewen@qq.com>
2019-03-15 16:57:36 +0800

    添加环境变量，添加scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/reset.css';
+import './styles/common.scss';
+import './styles/variables.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
